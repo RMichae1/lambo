467751676bd5abc659550bbb9dfd8835ad1d02a7
Richard Michael <r.michael@posteo.net>
2023-10-04 10:19:31 +0200

    bugfix scoring mutant not WT, add feasibility check for foldx tasks

--- lambo/tasks/poli_tasks/poli_task.py
+++ lambo/tasks/poli_tasks/poli_task.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 from poli import objective_factory
 from poli.core.util.external_observer import ExternalObserver
+from poli.core.util.proteins.mutations import find_closest_wildtype_pdb_file_to_mutant
 
 from lambo.candidate import StringCandidate
 from lambo.tasks.base_task import BaseTask
@@ -32,26 +33,27 @@ class PoliTask(BaseTask):
     def task_setup(self, config, project_root=None, *args, **kwargs):
         global problem_information, f, x0, y0, run_info
         # NOTE: when registering a poli task in hydra, we'll expect that the value to this key can be passed to poli
-        problem_name = config["task"][POLI_TASK_HYDRA_KEY]
+        self.problem_name = config["task"][POLI_TASK_HYDRA_KEY]
         seed = config.trial_id
         batch_size = config.task.batch_size
         self.num_start_examples = config.task.num_start_examples
         self.data_path = config.task.data_path
+        self.assets_pdb_path = None
         caller_info = {
             ALGORITHM: "LAMBO",
             STARTING_N: self.num_start_examples,
             BATCH_SIZE: batch_size,
             }
-        if problem_name == "foldx_stability_and_sasa": # if additional data i.e. PDBs are needed
-            assets_pdb_path = list(Path(self.data_path).glob("*/wt_input_Repair.pdb"))
-            if self.num_start_examples > len(assets_pdb_path): # if less data than allowed observations:
-                self.num_start_examples = len(assets_pdb_path)
-                caller_info[STARTING_N] = len(assets_pdb_path)
+        if self.problem_name == "foldx_stability_and_sasa": # if additional data i.e. PDBs are needed
+            self.assets_pdb_path = list(Path(self.data_path).glob("*/wt_input_Repair.pdb"))
+            if self.num_start_examples > len(self.assets_pdb_path): # if less data than allowed observations:
+                self.num_start_examples = len(self.assets_pdb_path)
+                caller_info[STARTING_N] = len(self.assets_pdb_path)
             problem_information, f, x0, y0, run_info = objective_factory.create(
-            name=problem_name,
+            name=self.problem_name,
             seed=seed,
             caller_info=caller_info,
-            wildtype_pdb_path=assets_pdb_path,
+            wildtype_pdb_path=self.assets_pdb_path,
             observer=self.observer,
             force_register=True,
             parallelize=self.poli_parallel, # NOTE: factory foldx parallelization
@@ -60,7 +62,7 @@ class PoliTask(BaseTask):
             )
         else: # Base case
             problem_information, f, x0, y0, run_info = objective_factory.create(
-                name=problem_name,
+                name=self.problem_name,
                 seed=seed,
                 caller_info=caller_info,
                 observer=self.observer,
@@ -111,14 +113,32 @@ class PoliTask(BaseTask):
     def score(self, candidates):
         y = np.zeros([len(candidates), y0.shape[1]])
         for i in range(len(candidates)):
-            seq = candidates[i].wild_residue_seq.lower()
+            seq = candidates[i].mutant_residue_seq.lower()
             y[i, ...] = f(np.atleast_2d(list(seq)))
             self.observer.observe(
                 np.array([candidates[i].mutant_residue_seq]), y[i:i+1, ...]
             )
-            # lambo appends bof and eof tokens
-            # y[i] = f(np.array([self.alphabet[seq[n]] for n in range(1, len(seq)-1)]).reshape(1, -1))
         return y
+    
+
+    def is_feasible(self, candidates):
+        if self.max_len is None:
+            is_feasible = np.ones(candidates.shape).astype(bool)
+        else:
+            is_feasible = np.array([len(cand) <= self.max_len for cand in candidates]).reshape(-1)
+        if self.asset_pdb_path is not None: 
+            # if we're working with PDB and FoldX we have to check feasibility
+            is_feasible = []
+            for candidate in candidates:
+                try:
+                    _pdb = find_closest_wildtype_pdb_file_to_mutant(self.assets_pdb_path, candidate.mutant_residue_seq.lower())
+                except ValueError:
+                    is_feasible.append(0)
+                    continue
+                is_feasible.append(1)
+            is_feasible = np.array(is_feasible).astype(bool)
+        assert is_feasible.shape[0] == candidates.shape[0]
+        return is_feasible
     
     def __del__(self):
         """
